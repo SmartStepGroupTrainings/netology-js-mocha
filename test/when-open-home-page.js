f4e3de461bbebaf5cb8b567d0d4d70b692d9ef32
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2016-10-11 01:01:03 +0300

    add test for server.js api calls

--- test/when-open-home-page.js
+++ test/when-open-home-page.js
@@ -3,16 +3,26 @@ var assert = require("chai").assert;
 var server = supertest.agent("http://localhost:3000");
 
 describe.only("When open home page", function() {
-      it("should return hello message", function(done) {
+    it("should return code 200 and hello message", function(done) {
         server
             .get("/")
             .expect("Content-type", /json/)
             .expect(200)
-            .end(function(err, res) {
-                assert.equal(res.status, 200);
-                assert.isFalse(res.body.error);
+            .end(function(error, response) {
+                assert.equal(response.status, 200);
+                assert.isFalse(response.body.error);
+                assert.equal(response.body.message, "Hello my friend!");
                 done();
             });
     });
 
+    it('should return code 404 if accessing random url', function(done) {
+        server
+            .get("/random")
+            .expect(404)
+            .end(function(error, response) {
+                assert.equal(response.status, 404);
+                done();
+            });
+    });
 });
