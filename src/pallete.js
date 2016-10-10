237233af74392a369c5f805d242cf7f2ea59cfa8
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2016-10-10 23:32:10 +0300

    Async test for getPallete()

--- src/pallete.js
+++ src/pallete.js
@@ -4,6 +4,13 @@ var Pallete = function() {
     self.getPallete = function() {
         return ["red", "green", "blue"];
     };
+
+
+    self.getPalleteAsync = function(callback) {
+        setTimeout(function() {
+            callback (["red", "green", "blue"]);
+        }, 100);
+    };
 };
 
 module.exports = Pallete;
--- test/pallete-tests.js
+++ test/pallete-tests.js
@@ -2,11 +2,20 @@ var Pallete = require("../src/pallete");
 var assert = require("assert");
 
 describe('Pallete should', function() {
-  it('return default collection with 3 colors', function() {
-    var pallete = new Pallete();
+    it('return default collection with 3 colors', function() {
+        var pallete = new Pallete();
 
-    var colors = pallete.getPallete();
+        var colors = pallete.getPallete();
 
-    assert.deepEqual(colors, ["red", "green", "blue"]);
-  });
+        assert.deepEqual(colors, ["red", "green", "blue"]);
+    });
+
+    it('async return default collection with 3 colors', function(done) {
+        var pallete = new Pallete();
+
+        var colors = pallete.getPalleteAsync(function(colors) {
+            assert.deepEqual(colors, ["red", "green", "blue"]);
+            done();
+        });
+    });
 });
